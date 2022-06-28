969eb026d63bfdf8cf898566fe36249181de1c2c
username1565 <39200328+username1565@users.noreply.github.com>
2022-06-28 15:53:56 +0300

    Add TcpServer.cs
    source: https://otus.ru/nest/post/350/

---
+++ TcpServer.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+using System.Net;      // потребуется
+using System.Net.Sockets;    // потребуется
+    class Program
+    {
+        static void Main()
+        {
+            // устанавливаем IP-адрес сервера и номер порта 1234
+            TcpListener server = new TcpListener(IPAddress.Any, 1234);  
+            server.Start();  // запускаем сервер
+            while (true)   // бесконечный цикл обслуживания клиентов
+            {
+                TcpClient client = server.AcceptTcpClient();  // ожидаем подключение клиента
+                NetworkStream ns = client.GetStream(); // для получения и отправки сообщений
+                byte[] hello = new byte[100];   // любое сообщение должно быть сериализовано
+                hello = Encoding.Default.GetBytes("hello world");  // конвертируем строку в массив байт
+
+                ns.Write(hello, 0, hello.Length);     // отправляем сообщение
+                while (client.Connected)  // пока клиент подключен, ждем приходящие сообщения
+                {
+                    byte[] msg = new byte[1024];     // готовим место для принятия сообщения
+                    int count = ns.Read(msg, 0, msg.Length);   // читаем сообщение от клиента
+                    Console.Write(Encoding.Default.GetString(msg, 0, count)); // выводим на экран полученное сообщение в виде строки
+                }
+            }
+        }
+    }

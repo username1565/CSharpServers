e9a7fa16248d3387522fafadac1de888aca8427d
username1565 <39200328+username1565@users.noreply.github.com>
2022-06-28 16:02:31 +0300

    Changes
    Add UDP support, according this source: https://stackoverflow.com/questions/46882815/c-sharp-sockets-tcp-udp/46927710#46927710

--- ConsoleClient.cs
+++ ConsoleClient.cs
@@ -1,15 +1,27 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
- 
-namespace ConsoleClient
+using System.Threading.Tasks;
+
+namespace TCPUDPClient
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        private static int port = 8888;
-        const string address = "127.0.0.1";
-        static void Main(string[] args)
-        {
+      UdpClient      udpClient = null;
+      TcpClient      tcpClient = null;
+      NetworkStream  tcpStream = null;
+      int            port      = 8081;
+      ConsoleKeyInfo key;
+      bool           run = true;
+      byte[]         buffer;
+
+      Console.WriteLine(string.Format("Starting TCP and UDP clients on port {0}...", port));
+
 			//program accept port, in args[0]
 			if(args.Length>=1){
 				int TryGetPort = 0;
@@ -19,49 +31,131 @@ namespace ConsoleClient
 				}
 				Console.WriteLine("port: "+port);
 			}
-			
+
             Console.Write("Введите свое имя:");
             string userName = Console.ReadLine();
-            TcpClient client = null;
-            try
-            {
-                client = new TcpClient(address, port);
-                NetworkStream stream = client.GetStream();
- 
-                while (true)
-                {
-                    Console.Write(userName + ": ");
-                    // ввод сообщения
-                    string message = Console.ReadLine();
-                    message = String.Format("{0}: {1}", userName, message);
-                    // преобразуем сообщение в массив байтов
-                    byte[] data = Encoding.Unicode.GetBytes(message);
-                    // отправка сообщения
-                    stream.Write(data, 0, data.Length);
- 
+
+      try
+      {
+        udpClient = new UdpClient();
+        udpClient.Connect(IPAddress.Loopback, port);
+
+        tcpClient = new TcpClient();
+        tcpClient.Connect(IPAddress.Loopback, port);
+		
+		bool udp = false;	//true - udp, false - tcp;
+
+        while(run)
+        {
+		
+          Console.WriteLine("Press 'T' for TCP sending, 'U' for UDP sending or 'CTRL+C' to exit.");
+          key = Console.ReadKey(true);
+		
+          switch (key.Key)
+          {
+            case ConsoleKey.U:
+				Console.WriteLine("UDP protocol selected");
+				udp = true;
+				break;
+			case ConsoleKey.T:
+				Console.WriteLine("TCP protocol selected");
+				udp = false;
+				break;
+		  }
+
+			string	message = Console.ReadLine();
+			message = String.Format("{0}, ({1}): {2}", DateTime.Now.ToString("HH:mm:ss.fff"), userName, message);
+			//buffer = Encoding.ASCII.GetBytes(DateTime.Now.ToString("HH:mm:ss.fff"));
+			buffer = Encoding.Unicode.GetBytes(message);
+			
+
+		if(udp == true){
+			udpClient.Send(buffer, buffer.Length);
+
+			IPEndPoint remoteEP = null; // адрес входящего подключения
+			buffer   = udpClient.Receive(ref remoteEP);
+
+			if (buffer != null && buffer.Length > 0)
+			{
+				//Console.WriteLine("UDP: " + Encoding.ASCII.GetString(buffer));
+				Console.WriteLine("UDP - " + Encoding.Unicode.GetString(buffer));
+			}			  
+		}
+		else{
+              if (tcpStream == null)
+                tcpStream = tcpClient.GetStream();
+
+              tcpStream.Write(buffer, 0, buffer.Length);
+			  
                     // получаем ответ
-                    data = new byte[64]; // буфер для получаемых данных
+                    byte[] data = new byte[64]; // буфер для получаемых данных
                     StringBuilder builder = new StringBuilder();
                     int bytes = 0;
                     do
                     {
-                        bytes = stream.Read(data, 0, data.Length);
+                        bytes = tcpStream.Read(data, 0, data.Length);
                         builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
                     }
-                    while (stream.DataAvailable);
+                    while (tcpStream.DataAvailable);
  
                     message = builder.ToString();
-                    Console.WriteLine("Сервер: {0}", message);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                client.Close();
-            }
+                    Console.WriteLine("TCP - " + message);
+			  
+		}
+		
+/*
+          Console.WriteLine("Press 'T' for TCP sending, 'U' for UDP sending or 'X' to exit.");
+          key = Console.ReadKey(true);
+
+          switch (key.Key)
+          {
+            case ConsoleKey.X:
+              run = false;
+              break;
+
+            case ConsoleKey.U:
+              buffer = Encoding.ASCII.GetBytes(DateTime.Now.ToString("HH:mm:ss.fff"));
+              udpClient.Send(buffer, buffer.Length);
+
+			IPEndPoint remoteEP = null; // адрес входящего подключения
+          buffer   = udpClient.Receive(ref remoteEP);
+
+          if (buffer != null && buffer.Length > 0)
+          {
+            Console.WriteLine("UDP: " + Encoding.ASCII.GetString(buffer));
+          }			  
+              break;
+
+            case ConsoleKey.T:
+              buffer = Encoding.ASCII.GetBytes(DateTime.Now.ToString("HH:mm:ss.fff"));
+
+              if (tcpStream == null)
+                tcpStream = tcpClient.GetStream();
+
+              tcpStream.Write(buffer, 0, buffer.Length);
+            break;
+          }
+*/		  
         }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("Main exception: " + ex);
+      }
+      finally
+      { 
+        if(udpClient != null)
+          udpClient.Close();
+
+        if(tcpStream != null)
+          tcpStream.Close();
+
+        if(tcpClient != null)
+          tcpClient.Close();
+      }
+
+      Console.WriteLine("Press <ENTER> to exit.");
+      Console.ReadLine();
     }
+  }
 }
--- ConsoleServer.cs
+++ ConsoleServer.cs
@@ -1,10 +1,13 @@
 using System;
-using System.Net; //IPAddress
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
-namespace ConsoleServer
+namespace TCPUDPServer
 {
     public class ClientObject
     {
@@ -35,9 +38,10 @@ namespace ConsoleServer
  
                     string message = builder.ToString();
  
-                    Console.WriteLine(message);
+                    Console.WriteLine("TCP: " + message);
                     // отправляем обратно сообщение в верхнем регистре
-                    message = message.Substring(message.IndexOf(':') + 1).Trim().ToUpper();
+                    //message = message.Substring(message.IndexOf(':') + 1).Trim().ToUpper();
+                    message = message.Substring(message.IndexOf(':') + 1).Trim();
                     data = Encoding.Unicode.GetBytes(message);
                     stream.Write(data, 0, data.Length);
                 }
@@ -56,12 +60,17 @@ namespace ConsoleServer
         }
     }
 	
-    class Program
+  public class TCPUDPServer
+  {
+	public static Encoding latin1 = Encoding.GetEncoding("ISO-8859-1");	//binary encoding can encode all bytes as string, and decode it back
+
+	public static void Main(string[] args)
     {
-        private static int port = 8888;
-        static TcpListener listener;
-        static void Main(string[] args)
-        {
+	
+      TcpListener tcpServer = null;
+      UdpClient   udpServer = null;
+      int         port      = 8082;
+
 			//program accept port, in args[0]
 			if(args.Length>=1){
 				int TryGetPort = 0;
@@ -71,32 +80,125 @@ namespace ConsoleServer
 				}
 				Console.WriteLine("port: "+port);
 			}
+	  
+      Console.WriteLine(string.Format("Starting TCP and UDP servers on port {0}...", port));
 
-            try
+      try
+      {
+        udpServer = new UdpClient(port);
+        tcpServer = new TcpListener(IPAddress.Any, port);
+
+        var udpThread          = new Thread(new ParameterizedThreadStart(UDPServerProc));
+        udpThread.IsBackground = true;
+        udpThread.Name         = "UDP server thread";
+        udpThread.Start(udpServer);
+
+        var tcpThread          = new Thread(new ParameterizedThreadStart(TCPServerProc));
+        tcpThread.IsBackground = true;
+        tcpThread.Name         = "TCP server thread";
+        tcpThread.Start(tcpServer);
+
+        Console.WriteLine("Press <ENTER> to stop the servers.");
+        Console.ReadLine();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("Main exception: " + ex);
+      }
+      finally
+      {
+        if (udpServer != null)
+          udpServer.Close();
+
+        if (tcpServer != null)
+          tcpServer.Stop();
+      }
+
+      Console.WriteLine("Press <ENTER> to exit.");
+      Console.ReadLine();
+    }
+
+    private static void UDPServerProc(object arg)
+    {
+      Console.WriteLine("UDP server thread started");
+
+      try
+      {
+        UdpClient server = (UdpClient)arg;
+        IPEndPoint remoteEP;
+        byte[] buffer;
+
+        for(;;)
+        {
+          remoteEP = null;
+          buffer   = server.Receive(ref remoteEP);
+
+          if (buffer != null && buffer.Length > 0)
+          {
+            //Console.WriteLine("UDP: " + Encoding.ASCII.GetString(buffer));
+			Console.WriteLine("UDP: " + Encoding.Unicode.GetString(buffer));
+			server.Send(buffer, buffer.Length, remoteEP.Address.ToString(), remoteEP.Port); // отправка
+          }
+        }
+      }
+      catch (SocketException ex)
+      {
+        if(ex.ErrorCode != 10004) // unexpected
+          Console.WriteLine("UDPServerProc exception: " + ex);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("UDPServerProc exception: " + ex);
+      }
+
+      Console.WriteLine("UDP server thread finished");
+    }
+
+    private static void TCPServerProc(object arg)
+    {
+      Console.WriteLine("TCP server thread started");
+
+      try
+      {
+        TcpListener server = (TcpListener)arg;
+        //byte[]      buffer = new byte[2048];
+        //int         count; 
+
+        server.Start();
+
+        for(;;)
+        {
+          TcpClient client = server.AcceptTcpClient();
+
+/*
+          using (var stream = client.GetStream())
+          {
+            while ((count = stream.Read(buffer, 0, buffer.Length)) != 0)
             {
-                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
-                listener.Start();
-                Console.WriteLine("Ожидание подключений...");
- 
-                while(true)
-                {
-                    TcpClient client = listener.AcceptTcpClient();
-                    ClientObject clientObject = new ClientObject(client);
+              Console.WriteLine("TCP: " + Encoding.ASCII.GetString(buffer, 0, count));
+            }
+          }
+          client.Close();
+*/
+                    ClientObject clientObject = new ClientObject(client);	//and start work with client
  
                     // создаем новый поток для обслуживания нового клиента
-                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
-                    clientThread.Start();
-                }
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                if(listener!=null)
-                    listener.Stop();
-            }
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));	//in new thread
+                    clientThread.Start();														//start thread
+
         }
+      }
+      catch (SocketException ex)
+      {
+        if (ex.ErrorCode != 10004) // unexpected
+          Console.WriteLine("TCPServerProc exception: " + ex);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("TCPServerProc exception: " + ex);
+      }
+
+      Console.WriteLine("TCP server thread finished");
     }
+  }
 }

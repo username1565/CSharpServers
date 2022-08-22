95d2d75790923c103a01c95544ae9be4051f8033
username1565 <39200328+username1565@users.noreply.github.com>
2022-08-22 13:04:09 +0300

    UdpClient -> UdpClients
    Many UDPClients, in separate threads.

--- ProgramUdpClients.cs
+++ ProgramUdpClients.cs
@@ -6,22 +6,27 @@ namespace Program
 {
 	partial class Program
 	{
-		static void Main6(string[] args)
-		{
+		static void Main(string[] args)
+		{		
 			string ServerUdpIP = "127.0.0.1";
 			int ServerUdpPort = 8081;
 			
 			if(args.Length == 1){
 				ServerUdpPort = System.Int32.Parse(args[0]);
 			}
-			else if(args.Length == 2){
+			if(args.Length == 2){
 				ServerUdpIP = args[0];
 				ServerUdpPort = System.Int32.Parse(args[1]);
 			}
 
 			try{
-				UDP.Client.Send(ServerUdpIP, ServerUdpPort, "test");
-				UDP.Client.Send(ServerUdpIP, ServerUdpPort, new byte[]{0,1,2,3,4,5});
+				UDP.Client udpClient = new UDP.Client(ServerUdpIP, ServerUdpPort);
+				udpClient.Send("test");
+				udpClient.Send(new byte[]{0,1,2,3,4,5});
+
+				UDP.Client udpClient2 = new UDP.Client(ServerUdpIP, ServerUdpPort+1);
+				udpClient2.Send("test");
+				udpClient2.Send(new byte[]{0,1,2,3,4,5});
 			}
 			catch(Exception ex){
 				Console.WriteLine(ex);

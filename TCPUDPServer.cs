a2e8dcd3a9d11d9f490ca95298b4682d397cda9c
username1565 <39200328+username1565@users.noreply.github.com>
2022-08-22 13:39:03 +0300

    TCPUDPMulticastServers
    Many TCP and UDP servers (with multicast), in separate threads.

---
+++ TCPUDPServer.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace TCPUDP
+{
+	class Server
+	{
+		public static void Main(string[] args){
+			string TcpIP;
+			string UdpIP;
+			int TcpPort = 0;
+			int UdpPort = 0;
+			string MultiCastGroupIP = "235.5.5.11";
+			
+			if(args.Length == 1){
+				TcpIP = UdpIP = "0.0.0.0";
+				TcpPort = UdpPort = System.Int32.Parse(args[0]);
+			}
+			if(args.Length == 2){
+				TcpIP = UdpIP = args[0];
+				TcpPort = UdpPort = System.Int32.Parse(args[1]);
+			}
+			if(args.Length == 3){
+				TcpIP = UdpIP = args[0];
+				TcpPort = UdpPort = System.Int32.Parse(args[1]);
+				MultiCastGroupIP = "235.5.5.11";
+			}
+			else{
+				TcpIP = UdpIP = "0.0.0.0";
+				TcpPort = UdpPort = 8081;
+			}
+
+			//Start TCP Servers
+			TCP.Server.Start(new string[]{TcpIP, TcpPort.ToString()});
+			TCP.Server.Start(new string[]{TcpIP, (TcpPort+1).ToString()});
+
+			//Start UDP Servers
+			UDP.Server.Start(UdpIP, UdpPort, MultiCastGroupIP);
+			UDP.Server.Start(UdpIP, UdpPort+1, MultiCastGroupIP);
+
+			Console.ReadLine();											//do not close window, and wait input
+		}
+	}
+}
---
+++ TCPUDPServers.sh
@@ -0,0 +1,2 @@
+csc /out:TCPUDPServers.exe *.cs
+mono TCPUDPServers.exe 0.0.0.0 8081 235.5.5.11

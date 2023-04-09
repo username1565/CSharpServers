adac30b0dc019e622ef55da3177b127adc768265
username1565 <39200328+username1565@users.noreply.github.com>
2023-04-09 22:05:08 +0300

    Update UdpClientsMulticast
    Update UdpClientsMulticast

--- UDPClients.bat
+++ UDPClients.bat
@@ -2,7 +2,7 @@ set fdir=%WINDIR%\Microsoft.NET\Framework
 set csc=%fdir%\v4.0.30319\csc.exe
 set msbuild=%fdir%\v4.0.30319\msbuild.exe
 
-set program=UdpClient
+set program=UdpClients
 
 %csc% /out:%program%.exe *.cs
 
@@ -10,6 +10,6 @@ set program=UdpClient
 ::%program%.exe 127.0.0.1 8081
 
 ::UDP Multicast - response not receiving...
-%program%.exe 127.0.0.1 8081 235.5.5.11
+%program%.exe 0.0.0.0 8081 235.5.5.11
 
 pause

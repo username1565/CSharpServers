77eb4931049b8a6b260fce3e335ba9199234f2a5
username1565 <39200328+username1565@users.noreply.github.com>
2023-04-10 05:32:40 +0300

    Rename Main, because it's in another file
    Rename Main, because it's in another file

--- ProgramUdpServer.cs
+++ ProgramUdpServer.cs
@@ -5,7 +5,7 @@ namespace Program
 	partial class Program
 	{
 		//start one UDP-server
-		static void Main(string[] args)
+		static void Main9(string[] args)
 		{
 			string IP = "0.0.0.0";
 			int port = 8081;

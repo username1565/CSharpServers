b05f53f1093a74c75a5ff7d15f086caa3b8cd66e
username1565 <39200328+username1565@users.noreply.github.com>
2023-04-15 11:18:41 +0300

    Add files via upload

--- TCPUDPClients.bat
+++ TCPUDPClients.bat
@@ -2,14 +2,10 @@ set fdir=%WINDIR%\Microsoft.NET\Framework
 set csc=%fdir%\v4.0.30319\csc.exe
 set msbuild=%fdir%\v4.0.30319\msbuild.exe
 
-set program=UdpClients
+set program=TCPUDPClients
 
 %csc% /out:%program%.exe *.cs
 
-::UDP
-::%program%.exe 127.0.0.1 8081
-
-::UDP Multicast - response not receiving...
-%program%.exe 0.0.0.0 8081 235.5.5.11
+%program%.exe 127.0.0.1 8081 235.5.5.11
 
 pause

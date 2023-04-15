9c29dce4c349cb1ef6a0b362fc4bd29416415866
username1565 <39200328+username1565@users.noreply.github.com>
2023-04-15 09:41:26 +0300

    Update UDPClientsMulticast
    Update UDPClientsMulticast

--- UDPClients.bat
+++ UDPClients.bat
@@ -2,7 +2,7 @@ set fdir=%WINDIR%\Microsoft.NET\Framework
 set csc=%fdir%\v4.0.30319\csc.exe
 set msbuild=%fdir%\v4.0.30319\msbuild.exe
 
-set program=UdpClient
+set program=UdpClients
 
 %csc% /out:%program%.exe *.cs
 
--- UDPClients.sh
+++ UDPClients.sh
@@ -1,2 +1,2 @@
-csc /out:UDPClient.exe *.cs
-mono UDPClient.exe 127.0.0.1 8081 235.5.5.11
+csc /out:UDPClients.exe *.cs
+mono UDPClients.exe 127.0.0.1 8081 235.5.5.11

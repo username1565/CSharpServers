d5ceb700bfe6ff93c12ec2effebbc4b3527733b0
username1565 <39200328+username1565@users.noreply.github.com>
2023-04-15 07:26:38 +0000

    Delete UDPServerMulticast.sh

--- UDPServerMulticast.sh
+++
@@ -1,6 +0,0 @@
-csc /out:UDPServerMulticast.exe *.cs
-#	UDP server without multicast group
-mono UDPServerMulticast.exe 0.0.0.0 8081
-
-#	UDP server with mulcicast group IP
-mono UDPServerMulticast.exe 0.0.0.0 8081 235.5.5.11

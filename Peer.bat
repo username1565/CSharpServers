e9a46e84798ab49a1e8ec3a8140466375f3f9903
username1565 <39200328+username1565@users.noreply.github.com>
2024-01-21 04:15:34 +0200

    Set sync intervals from cmd arguments
    DHT.cs - change value of SyncDHTInterval to 60 seconds.
    
    LocalPeerDiscovery.cs - change value of PeerDiscoveryInterval to 300 seconds, and run DiscoveryPeers(); before timer interval elapsed.
    
    Peer.cs - remove PeerDiscoveryInterval (this already exists as LocalPeersDiscovery.PeerDiscoveryInterval ), add DHT.DHT_client.SyncDHTInterval as argument, fix usage text, amd run LocalPeersDiscovery.RunDiscoveryPeersInterval() without any value (this value already defined or redefined frmo cmd arguments).
    
    Peer.bat, Peer.sh - add SyncDHTInterval as agrument.

--- Peer.bat
+++ Peer.bat
@@ -6,12 +6,12 @@ set program=Peer
 
 %csc% /out:%program%.exe *.cs /reference:Mono.Data.Sqlite.dll /reference:sqlite3.dll /platform:x86
 
-::Peer.exe IP port MulticastGroupIP PeerDiscoveryInterval DBFilePath TableName KeyName ValueName
+::Peer.exe IP port MulticastGroupIP PeerDiscoveryInterval SyncDHTInterval DBFilePath TableName KeyName ValueName
 
 ::SQLite3 storage
-%program%.exe 0.0.0.0 8081 235.5.5.11 15 Hashtable.db3 KeyValue key value
+%program%.exe 0.0.0.0 8081 235.5.5.11 15 30 Hashtable.db3 KeyValue key value
 
 ::Simple TXT storage
-::%program%.exe 0.0.0.0 8081 235.5.5.11 15 Hashtable.txt
+::%program%.exe 0.0.0.0 8081 235.5.5.11 15 30 Hashtable.txt
 
 pause
